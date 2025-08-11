8c7347d67eebc34a38795eb44ac9f7f8b88e5a35
nv3033 <116170942+nv3033@users.noreply.github.com>
2025-08-11 13:38:51 +0300

    minor changes

---
+++ dark_mantle/scripts/vopros_switch.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@export var rotation_speed = 50.0
+@onready var player_near = false
+@export var max_size = 0.398
+@export var scale_speed = 0.05
+@onready var player = $"../CharacterBody3D"
+@onready var switch = $"../switch"
+@onready var switch_pressed = false
+
+func _process(delta):
+	rotate_y(deg_to_rad(rotation_speed * delta))
+	if player_near == true and scale.x < max_size:
+		scale = Vector3(scale.x+scale_speed, scale.y+scale_speed, scale.z+scale_speed)
+	if player_near == false and scale.x > 0:
+		scale = Vector3(scale.x-scale_speed, scale.y-scale_speed, scale.z-scale_speed)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and player_near:
+		switch.rotate_x(deg_to_rad(180));
+		switch_pressed = !switch_pressed
+
+func _on_area_3d_2_body_exited(body: Node3D) -> void:
+	pass
+
+func _on_area_3d_2_body_entered(body: Node3D) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player") == true:
+		player_near = true
+		print("player entered")
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player") == true:
+		player_near = false
+		print("player exited")
